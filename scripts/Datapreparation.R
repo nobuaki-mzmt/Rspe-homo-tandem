072a427f08f2284943e19811d66fb7ab387fad83
nobuaki.mzmt <nobuaki.mzmt@gmail.com>
2021-07-12 13:36:58 +0900

    Project initiation -note that program initiate on 7/9, but it was not saved. -convert raw .csv data into .rda data, and output raw trajectories -scale .rda data and output trajectories and speed time development for checking tracking results.

---
+++ scripts/Datapreparation.R
@@ -0,0 +1,140 @@
+## R. speratus homosexual tandem analysis
+## N. Mizumoto
+## 7/9/2021 ~
+## script with R projects
+
+## Data preparation
+
+# Environment -------------------------------------------------------------
+{
+  RPROJ <- list(PROJHOME = normalizePath(getwd()))
+  attach(RPROJ)
+  rm(RPROJ)
+}
+
+# Setup -------------------------------------------------------------------
+{
+  rm(list = ls())
+  
+  today <- Sys.Date()
+  
+  ## Packages
+  {
+    library(data.table)
+    library(stringr)
+    require(grid)
+    require(gridExtra)
+  }
+  
+  ## parameters
+  fps <- 5
+  arena.size <- 140 # mm
+  
+  ## file location
+  dir.create(file.path(PROJHOME, "/data/", "rda"), showWarnings = FALSE)
+  rda.place <- file.path(PROJHOME, "/data/", "rda")
+}
+
+## Raw data use
+# reads all csv files and 1) output raw trajectories in .png file
+# 2) convert them in 5FPS and R data.frame saved in .rda files
+{
+  ## data
+  dir.create(file.path(PROJHOME, "/plot"), showWarnings = FALSE)
+  dir.create(file.path(PROJHOME, "/plot/raw-trajectories"), showWarnings = FALSE)
+  rawdata <- list.files(file.path(PROJHOME, "data/raw"), full.names = TRUE, pattern = ".csv")
+  dataname <- list.files(file.path(PROJHOME, "data/raw"), full.names = F, pattern = ".csv")
+  
+  ## options
+  options(warn = 0)
+  Plot <- F      # See overall trajectories
+  Dataframe <- T # Integrate all in one dataframe
+  
+  df <- data.frame()
+  ## plots
+  for(v in 1:length(rawdata)){
+    
+    # file info
+    d <- data.frame(fread(rawdata[v], header=T))
+    d <- d[1:(5*60*30),]
+    
+    species = substr(dataname[v], 1, 2)
+    treat = substr(dataname[v], 4, 5)
+    role = substr(dataname[v], 7, 7)
+    if(role=="F"){role ="Follower"} else {role="Leader"}
+    id = substr(dataname[v], 9, 10)
+    scheme = substr(dataname[v], 12, 14) 
+    name <- paste(species, treat, id, role, sep="-")
+    print(paste(v, name))
+    
+    # plot
+    if(Plot){
+      png(file.path(PROJHOME, "plot/raw-trajectories/", str_replace(dataname[v], "csv", "png")))
+      par(mfrow=c(1,1), pin=c(3,2))
+      plot( d[,2], d[,3], type="l", col=1, xlim=c(0,1800), ylim=c(0,1200),
+            main=paste(name, scheme),
+            xlab="x(mm)", ylab="y(mm)")
+      dev.off()
+    }
+    
+    # datafrmae
+    d[,1] <- d[,1]/30
+    d <- d[seq(1,9000,6),]
+    if(Dataframe){
+      if(scheme == "tan"){
+        dftemp1 <- data.frame(species, treat, role, id, scheme = paste0(scheme, "-L"), name, time = d[,1], x=d[,2], y=d[,3])
+        df <- rbind(df,dftemp1)
+        dftemp2 <- data.frame(species, treat, role, id, scheme = paste0(scheme, "-F"), name, time = d[,1], x=d[,4], y=d[,5])
+        df <- rbind(df,dftemp2)
+      } else {
+        dftemp1 <- data.frame(species, treat, role, id, scheme = scheme, name, time = d[,1], x=d[,2], y=d[,3])
+        df <- rbind(df,dftemp1)
+      }
+    }
+  }
+  f.name <- paste0(rda.place, "/AllData.rda")
+  save(df, file = f.name)
+}
+
+## Scaling
+# assuming that termites moved throughout the arena, scale the movement range as arena size
+# scaling is performed for each observed arena (pool tandem and separation for each individual)
+# also check speed time development to detect tracking error
+{
+  Plot <- T
+  dir.create(file.path(PROJHOME, "/plot/scale-trajectories"), showWarnings = FALSE)
+  
+  ind.names <- unique(df$name) 
+  
+  for(v in 1:length(ind.names)){
+    df.temp <- df[df$name == ind.names[v],]
+    print(ind.names[v])
+    
+    x <- df.temp$x
+    y <- df.temp$y
+    xL <- max(x) - min(x)
+    yL <- max(y) - min(y)
+    df.temp$x <- (x-min(x))/xL * arena.size
+    df.temp$y <- (y-min(y))/yL * arena.size
+    
+    # plot
+    if(Plot){
+      p1 <- ggplot(data=df.temp, aes(x=x, y=y, col=as.factor(scheme), group=as.factor(scheme))) +
+        geom_path() + 
+        theme_bw() +
+        theme(legend.position = "none", aspect.ratio = 1)+ 
+        xlim(c(0,140)) + ylim(c(0,140)) +
+        ggtitle(paste(v, ind.names[v]))
+      df.temp$speed = c(NA, sqrt( diff(df.temp$x)^2 + diff(df.temp$y)^2 ) )
+      p2 <- ggplot(data=df.temp, aes(x=time, y=speed, col=as.factor(scheme), group=as.factor(scheme))) +
+        geom_path() + 
+        theme_bw() +
+        theme(legend.position = "none", aspect.ratio = 1)+ 
+        coord_cartesian(xlim=c(0,300), ylim=c(0, 30)) +
+        ggtitle(paste(v, ind.names[v]))
+      p3 <-grid.arrange(p1, p2, nrow = 1)
+      ggsave(file.path(PROJHOME, "plot/scale-trajectories/", paste0(df.temp$name[1], ".png")), p3, width = 6, height = 3)
+    }
+  }  
+  
+}
